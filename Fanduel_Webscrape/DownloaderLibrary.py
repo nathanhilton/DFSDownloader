003f38c0f42e89203137475cf6e110505597227a
nathanhilton <50312273+nathanhilton@users.noreply.github.com>
2021-02-28 20:54:59 -0500

    adding extra files

---
+++ Fanduel_Webscrape/DownloaderLibrary.py
@@ -0,0 +1,11 @@
+
+
+class downloaderBase():
+    def __init__(self, startDate, endDate, sport='NBA', platform='fanduel'):
+        self.startDate = startDate
+        self.endDate = endDate
+        self.sport = sport
+        self.platform = platform
+
+    def download(self):
+        pass
