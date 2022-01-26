f211156d5e2d280e71922e52024635d0b7a6c0c7
nathanhilton <50312273+nathanhilton@users.noreply.github.com>
2022-01-26 18:15:20 -0500

    skeleton

--- DFSDownloader/NFL/NFLDraftkings.py
+++ DFSDownloader/NFL/NFLDraftkings.py
@@ -1,9 +1,8 @@
-from DFSDownloader.NBA.NBADownloader import NBADownloader
 from DownloaderLibrary import downloaderBase
 from NFL.NFLDownloader import NFLDownloader
 
 
-class NFLDraftkings(NBADownloader):
+class NFLDraftkings(NFLDownloader):
     def __init__(self, startDate, endDate):
         NFLDownloader.__init__(self, 'dk')
         downloaderBase.__init__(self, startDate, endDate, 'NFL', 'draftkings')
